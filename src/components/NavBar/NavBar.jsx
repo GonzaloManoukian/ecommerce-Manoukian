213997ffc2d424637f62bea1f58814bf36edfaf2
Gonzalo Manoukian <gonzalomanoukian@gmail.com>
2021-03-11 16:56:56 -0300

    Actualizacion 11-03-21

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,27 @@
+import React from "react"
+import './navbar.css'
+
+const NavBar = () => {
+return (
+    <header>
+      <nav>
+        <ul>
+          <a href="#">
+            <li>Inicio</li>
+          </a>
+          <a href="#catalogo">
+            <li>Catálogo</li>
+          </a>
+          <a href="#">
+            <li>Contacto</li>
+          </a>
+        </ul>
+      </nav>
+
+    </header>
+)
+}
+
+export default NavBar;
+
+
