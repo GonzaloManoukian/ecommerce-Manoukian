186c494562a1fcba062c8845373f212b99a170d5
Gonzalo Manoukian <gonzalomanoukian@gmail.com>
2021-03-23 22:43:00 -0300

    Desafio 5

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from 'react'
 import ItemCount from "../ItemCount/ItemCount";
 import ItemList from "../ItemList/ItemList";
 
+
 const ItemListContainer = ({ greeting /* greeting:saludo Asigna greeting a una variable llamada saludo*/ }) => {
 
     const agregar = (num) => console.log(`Se Agrego un Item. Cantidad: ${num}`)
@@ -15,15 +16,15 @@ const ItemListContainer = ({ greeting /* greeting:saludo Asigna greeting a una v
 
             setTimeout(() => {
                 resolve([
-                    { imagen: "img/bukowskicartero.jpg", titulo: "Cartero", autor: "Charles Bukowski", precio: "1000", id: "1" },
-                    { imagen: "img/austerviajes.jpg", titulo: "Viajes por el scriptorum", autor: "Paul Auster", precio: "1400", id: "2" },
-                    { imagen: "img/abseda.jpg", titulo: "Seda", autor: "Alessandro Baricco", precio: "900", id: "3" }
+                    { imagen: "/public/assets/img/bukowskicartero.jpg", titulo: "Cartero", autor: "Charles Bukowski", precio: "1000", id: "1" },
+                    { imagen: "/public/assets/img/austerviajes.jpg", titulo: "Viajes por el scriptorum", autor: "Paul Auster", precio: "1400", id: "2" },
+                    { imagen: "/public/assets/img/abseda.jpg", titulo: "Seda", autor: "Alessandro Baricco", precio: "900", id: "3" }
                 ])
             }, 2000)
+        })
 
-            promesa.then((resultado) => {
-                setItems(resultado)
-            })
+        promesa.then((resultado) => {
+            setItems(resultado)
         })
     })
 
